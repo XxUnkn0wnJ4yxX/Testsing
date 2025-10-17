fe8470e0440fe6f475b295d74821ba9bb749029d
XxUnkn0wnJ4yxX <JamesMraz2@gmail.com>
2025-10-17 14:48:58 +0000

    rsehdtgh

--- javascript.js
+++ javascript.js
@@ -6,5 +6,5 @@ function newRect(e){
     var rect = new Rectangle(20, 30);
     rect.setPosition(e.getX() - 10, e.getY() - 15);
     rect.setColor(Randomizer.nextColor());
-    add(rect);
+    rect.draw();
 }
