674f69da7dcd03921317efc99720e2f5f4b08c22
XxUnkn0wnJ4yxX <JamesMraz2@gmail.com>
2026-03-11 21:19:42 +0000

    js finna make me coomit arson

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
+var BUTWIDTH = window.innerWidth;
+console.log(window.innerWidth)
 function setUp(){
 var but = document.getElementsByClassName("but");
 for(var i = 0; i < but.length; i++){
-    but[i].
-    // tryna change the style but it's being stupid
+    but[i].style.width = BUTWIDTH;
+    // tryna make button width the window width but js is being stupid
     console.log("setup repeat " + i)
 }
 }
