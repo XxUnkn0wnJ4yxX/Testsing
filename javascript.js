e40e8b306417e070a002e23386b1137fe9631897
XxUnkn0wnJ4yxX <JamesMraz2@gmail.com>
2026-03-12 21:34:40 +0000

    FRICK YOU JS

--- javascript.js
+++ javascript.js
@@ -1,14 +1,25 @@
-var BUTWIDTH = window.innerWidth;
+var butWidth = window.innerWidth - 15;
 console.log(window.innerWidth)
 function setUp(){
-var but = document.getElementsByClassName("but");
-for(var i = 0; i < but.length; i++){
-    but[i].style.width = BUTWIDTH;
-    // tryna make button width the window width but js is being stupid
-    console.log("setup repeat " + i)
-}
+buttonSetUp();
+setInterval(buttonSetUp, 100);
+var pageButtons = document.getElementById("pageBar");
+pageButtons.innerHTML = "
+<a href = 'index.html'><button class = 'pageS'>page 1</button></a>
+<a href = 'page2.html'><button class = 'pageS'>page 2</button></a>
+";
 }
 
+function buttonSetUp() {
+    var butWidth = window.innerWidth - 15;
+    var but = document.getElementsByClassName("but");
+    for(var i = 0; i < but.length; i++){
+        but[i].style.width = (butWidth) + "px";
+        // tryna make button width the window width but js is being stupid
+        console.log("setup repeat " + i);
+        console.log(but[i].style.width)
+    }
+}
 
 
 window.onload = setUp();
