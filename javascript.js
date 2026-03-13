7edefa0fbd1430f315a02ce0e78887a739f49cac
XxUnkn0wnJ4yxX <JamesMraz2@gmail.com>
2026-03-13 14:59:42 +0000

    gtukyhj

--- javascript.js
+++ javascript.js
@@ -1,25 +1,41 @@
-var butWidth = window.innerWidth - 15;
-console.log(window.innerWidth)
+var butWidth = window.innerWidth - 30;
+console.log(window.innerWidth);
+var but = document.getElementsByClassName("but");
+
 function setUp(){
+
+//page 2 buttons
+buttonLength();
+setInterval(buttonLength, 1000);
 buttonSetUp();
-setInterval(buttonSetUp, 100);
+
+//page bar
 var pageButtons = document.getElementById("pageBar");
-pageButtons.innerHTML = "
-<a href = 'index.html'><button class = 'pageS'>page 1</button></a>
-<a href = 'page2.html'><button class = 'pageS'>page 2</button></a>
-";
+pageButtons.innerHTML = ("<a href = 'index.html'><button class = 'pageS'>page 1</button></a><a href = 'page2.html'><button class = 'pageS'>page 2</button></a>");
 }
 
-function buttonSetUp() {
-    var butWidth = window.innerWidth - 15;
-    var but = document.getElementsByClassName("but");
+//sets up the buttons on page 2
+function buttonLength() {
+    //mkes buttons the right size
+    var butWidth = window.innerWidth - 30;
     for(var i = 0; i < but.length; i++){
         but[i].style.width = (butWidth) + "px";
-        // tryna make button width the window width but js is being stupid
-        console.log("setup repeat " + i);
-        console.log(but[i].style.width)
     }
 }
+
+function buttonSetUp () {
+    var right = Math.floor(Math.random() * but.length) + 1;
+    for(var i = 0; i < but.length; i++ ){
+        if(but[i] == but[right]){
+            but[i].id = "right";
+            console.log("button " + i + " is right")
+        } else {
+            but[i].id = "wrong";
+            console.log("button " + i + " is wrong")
+        }
+    }
+}
+
 
 
 window.onload = setUp();
