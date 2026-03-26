33d8d55a8119cf48326e90c2c9eee25db60f215b
XxUnkn0wnJ4yxX <JamesMraz2@gmail.com>
2026-03-26 14:51:58 +0000

    random color gen works

--- script3.js
+++ script3.js
@@ -3,20 +3,24 @@ var HEIGHT = window.innerHeight;
 
 function setUp() {
     var but = document.getElementById("coloredButton");
-    but.style.width = (WIDTH);
+    but.style.width = (WIDTH + "px");
+    but.style.height = (HEIGHT + "px");
 
-    but.style.backgroundColor = randomColor();
+    but.style.backgroundColor = (randomColor());
 }
 
 function randomColor() {
     var color = "#";
     var possibleVars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"];
     console.log(possibleVars);
-    for(var i = 0; i < 17; i++) {
-        var listNum = Math.random() * 16;
-        color = color += listNum;
+    for(var i = 0; i < 6; i++) {
+        var rawListNum = Math.random() * 16;
+        var listNum = Math.floor(rawListNum);
+        console.log(listNum)
+        color = color += possibleVars[listNum];
         console.log(color);
     }
+    return(color)
 }
 
 window.onload = setUp();
