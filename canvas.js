2ced9776d67484cff3b75c83658577f33e773fdc
XxUnkn0wnJ4yxX <JamesMraz2@gmail.com>
2026-04-09 14:22:20 +0000

    bn,.kj n;kj.nmkl

---
+++ canvas.js
@@ -0,0 +1,12 @@
+var WIDTH = window.innerWidth;
+
+
+
+function setUp() {
+    var canvas = document.getElementById("canvas");
+    var can = canvas.getContext("2d");
+    can.fillStyle = "#ff0000";
+    can.fillRect(20,20,20,20);
+}
+
+window.onload = setUp;
