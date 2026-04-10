806c6c848b972c4103e5fb2b0d08b1fb6313c1e4
XxUnkn0wnJ4yxX <JamesMraz2@gmail.com>
2026-04-10 20:11:12 +0000

    gfhyhrcg

--- canvas.js
+++ canvas.js
@@ -5,8 +5,15 @@ var WIDTH = window.innerWidth;
 function setUp() {
     var canvas = document.getElementById("canvas");
     var can = canvas.getContext("2d");
+
     can.fillStyle = "#ff0000";
     can.fillRect(20,20,20,20);
-}
 
-window.onload = setUp;
+    canvas.addEventListener("keydown", function(event) {
+        console.log(event.code);
+        if(event.code === Enter){
+                can.fillStyle = "#0000ff";
+                can.fillRect(30,30,20,20);
+        }
+    });
+}
