3fb1bc1e3e368a4d8a88cefc3ceaabe3af853ae8
XxUnkn0wnJ4yxX <JamesMraz2@gmail.com>
2026-04-15 14:48:31 +0000

    y6ndruf

--- script4.js
+++ script4.js
@@ -1,41 +1,109 @@
-var ONE = false;
-var TWO = false;
-var THREE = false;
+var one = false;
+var two = false;
+var three = false;
 
 
 function setUp(){
 
-//page bar
-var pageButtons = document.getElementById("pageBar");
-pageButtons.innerHTML = ("<a href = 'index.html'><button class = 'pageS'>page 1</button></a><a href = 'page2.html'><button class = 'pageS'>page 2</button></a><a href = 'page3.html'><button class = 'pageS'>Page 3</button></a><a href = 'page4.html'><button class = 'pageS'>Page 4</button></a>");
+    var pageButtons = document.getElementById("pageBar");
+    pageButtons.innerHTML = ("<a href = 'index.html'><button class = 'pageS'>page 1</button></a><a href = 'page2.html'><button class = 'pageS'>page 2</button></a><a href = 'page3.html'><button class = 'pageS'>Page 3</button></a><a href = 'page4.html'><button class = 'pageS'>Page 4</button></a>");
 
-var colorsint = setInterval(() => {
-    var htmll = document.getElementsByTagName("html");
-htmll[0].style.backgroundColor = randomColor();
-}, 500);
+    var colorsint = setInterval(() => {
+        var htmll = document.getElementsByTagName("html");
+        var col = randomColor();
+        htmll[0].style.backgroundColor = col;
+        var nexxt = document.getElementById("next");
+        nexxt.style.backgroundColor = col
 
-var stopBut = document.getElementById("stopp");
-stopBut.onclick = function() {
-    stopColor(colorsint);
-};
+    }, 500);
 
-var but1 = document.getElementById("but1");
-var but2 = document.getElementById("but2");
-var but3 = document.getElementById("but3");
+    var stopBut = document.getElementById("stopp");
+    stopBut.onclick = function() {
+        stopColor(colorsint);
+    };
 
-but1.onclick = oneclick;
-but2.onclick = twoclick;
-but3.onclick = threeclick;
+    var but1 = document.getElementById("but1");
+    var but2 = document.getElementById("but2");
+    var but3 = document.getElementById("but3");
+
+    but1.onclick = oneclick;
+    but2.onclick = twoclick;
+    but3.onclick = threeclick;
 }
 
 function oneclick(){
+    var but1 = document.getElementById("but1");
+    var but2 = document.getElementById("but2");
+    if(one == true){
+        one = false;
+        but1.style.backgroundColor = "#000000"
+    }else{
+        one = true;
+        but1.style.backgroundColor = "#dddddd"
+    }
+    if(two == true){
+        two = false;
+        but2.style.backgroundColor = "#000000"
+    }else{
+        two = true;
+        but2.style.backgroundColor = "#dddddd"
+    }
+    if(three == true){
 
+    }else{
+        
+    }
+    if(one == true && two == true && three == true){
+
+        var nexxt = document.getElementById("next");
+        nexxt.style.visibility = "visible";
+        
+    }
 }
 function twoclick(){
-    
+    var but2 = document.getElementById("but2");
+    var but3 = document.getElementById("but3");
+      if(one == true){
+
+    }else{
+
+    }
+    if(two == true){
+        two = false;
+        but2.style.backgroundColor = "#000000"
+    }else{
+        two = true
+        but2.style.backgroundColor = "#dddddd"
+    }
+    if(three == true){
+        three = false
+        but3.style.backgroundColor = "#000000"
+    }else{
+        three = true
+        but3.style.backgroundColor = "#dddddd"
+    }  
 }
 function threeclick(){
-    
+    var but1 = document.getElementById("but1");
+    var but3 = document.getElementById("but3");
+    if(one == true){
+        one = false;
+        but1.style.backgroundColor = "#000000"
+    }else{
+
+    }
+    if(two == true){
+
+    }else{
+        
+    }
+    if(three == true){
+        three = false;
+        but3.style.backgroundColor = "#000000"
+    }else{
+        three = true
+        but3.style.backgroundColor = "#dddddd"
+    }
 }
 
 function stopColor(int){
